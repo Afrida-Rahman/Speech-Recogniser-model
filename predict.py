9dab9ca25e91173c53a4b8679c888702537da525
AfridaRSam <afrida.r.samma@gmail.com>
2021-04-23 16:21:45 +0600

    add test files

--- predict.py
+++ predict.py
@@ -5,6 +5,7 @@ import numpy as np
 SAVED_MODEL_PATH = "model.h5"
 SAMPLES_TO_CONSIDER = 22050
 
+
 class _Keyword_Spotting_Service:
     model = None
     _mapping = [
@@ -23,7 +24,6 @@ class _Keyword_Spotting_Service:
 
     def predict(self, file_path):
         MFCCs = self.preprocess(file_path)
-
         MFCCs = MFCCs[np.newaxis, ..., np.newaxis]
 
         predictions = self.model.predict(MFCCs)
@@ -31,15 +31,13 @@ class _Keyword_Spotting_Service:
         predicted_keyword = self._mapping[predicted_index]
         return predicted_keyword
 
-
     def preprocess(self, file_path, num_mfcc=13, n_fft=2048, hop_length=512):
         signal, sample_rate = librosa.load(file_path)
 
         if len(signal) >= SAMPLES_TO_CONSIDER:
             signal = signal[:SAMPLES_TO_CONSIDER]
 
-        MFCCs = librosa.feature.mfcc(signal, sample_rate, n_mfcc=num_mfcc, n_fft=n_fft,
-                                         hop_length=hop_length)
+            MFCCs = librosa.feature.mfcc(signal, sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
         return MFCCs.T
 
 
@@ -50,10 +48,11 @@ def Keyword_Spotting_Service():
     return _Keyword_Spotting_Service._instance
 
 
-
-
 if __name__ == "__main__":
-
     kss = Keyword_Spotting_Service()
-    keyword = kss.predict("test/no.wav")
-    print(f"predicted keywords: {keyword}")
+    kss1 = Keyword_Spotting_Service()
+
+    assert kss is kss1
+
+    keyword = kss.predict("test/down.wav")
+    print(keyword)
